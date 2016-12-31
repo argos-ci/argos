353db27467ef3e7912a3048cf7166f8036961b8e
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-12-31 17:39:13 +0100

    [server] Try having something live (#12)

--- src/review/webpack/browserProductionConfig.js
+++ src/review/webpack/browserProductionConfig.js
@@ -1,17 +1,22 @@
-import webpack from 'webpack';
+// import webpack from 'webpack';
+import AssetsPlugin from 'assets-webpack-plugin';
 import browserBaseConfig from './browserBaseConfig';
 
 export default {
   ...browserBaseConfig,
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-      },
-      output: {
-        comments: false,
-      },
+    new AssetsPlugin({
+      filename: './server/static/review/assets.json',
+      prettyPrint: true,
     }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false,
+    //     screw_ie8: true,
+    //   },
+    //   output: {
+    //     comments: false,
+    //   },
+    // }),
   ],
 };
---
+++ src/server/rendering.js
@@ -0,0 +1,57 @@
+// @flow weak
+/* eslint-disable no-console */
+
+import ejs from 'ejs';
+import fs from 'fs';
+import path from 'path';
+import { minify } from 'html-minifier';
+
+let htmlWebpackPlugin;
+let indexString = fs.readFileSync(path.join(__dirname, '../review/index.ejs'), 'UTF-8');
+
+if (process.env.NODE_ENV === 'production') {
+  const assets = require('../../server/static/review/assets.json');
+
+  indexString = minify(indexString, {
+    collapseWhitespace: true,
+    removeComments: true,
+    minifyJS: true,
+  });
+
+  htmlWebpackPlugin = {
+    files: {
+      css: [assets.main.css],
+      js: [assets.main.js],
+    },
+  };
+} else {
+  htmlWebpackPlugin = {
+    files: {
+      js: ['/browser.js'],
+    },
+  };
+}
+
+function isMediaBot(userAgent) {
+  let output = false;
+
+  if (userAgent && (
+    userAgent.indexOf('facebookexternalhit') !== -1 ||
+    userAgent.indexOf('Twitterbot') !== -1
+    )) {
+    output = true;
+  }
+
+  return output;
+}
+
+export default (req, res) => {
+  const output = ejs.render(indexString, {
+    cache: true,
+    filename: 'review/index.ejs',
+    isMediaBot: isMediaBot(req.headers['user-agent']),
+    htmlWebpackPlugin,
+  });
+
+  res.status(200).send(output);
+};
