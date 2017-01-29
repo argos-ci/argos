bf1ce8c608e38907b057f152876d8ac65f2f0cfa
Greg Bergé <neoziro@gmail.com>
2017-01-29 21:35:43 +0100

    chore(sentry): enable auto breadcrumbs

--- src/modules/crashReporter/crashReporter.js
+++ src/modules/crashReporter/crashReporter.js
@@ -2,23 +2,28 @@
 
 let raven
 let DSN
-let release
+let config
 
 if (process.env.PLATFORM === 'browser') {
   raven = require('raven-js')
   DSN = 'https://f1690f74cc6e432e922f32da3eb051c9@sentry.io/133417'
-  release = window.clientData.releaseVersion
+  config = {
+    release: window.clientData.releaseVersion,
+  }
 } else {
   raven = require('raven')
   DSN = 'https://261cb80891cb480fa452f7e18c0e57c0:dc050bb97a4d4692aa3e957c5c89d393@sentry.io/133418'
-  release = require('../../config').default.get('heroku', 'releaseVersion')
+  config = {
+    autoBreadcrumbs: true,
+    release: require('../../config').default.get('heroku', 'releaseVersion'),
+  }
 }
 
 const crashReporter = {
   init: () => {
     raven.config(DSN, {
       environment: process.env.NODE_ENV, // Should always be production
-      release,
+      ...config,
     })
 
     if (process.env.NODE_ENV === 'production') {
