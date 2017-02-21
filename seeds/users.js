44d061a87c36bf12a4ed150b27b216d9b4cd2751
Greg Bergé <neoziro@gmail.com>
2017-02-22 00:45:04 +0100

    chore: fix truncate and seed

--- seeds/users.js
+++ seeds/users.js
@@ -2,13 +2,24 @@ exports.seed = (knex, Promise) => {
   return knex('users').delete()
     .then(() => {
       return Promise.all([
-        knex('users').insert({
-          githubId: 3165635,
-          name: 'Olivier Tassinari',
-          email: 'olivier.tassinari@gmail.com',
-          createdAt: new Date().toISOString(),
-          updatedAt: new Date().toISOString(),
-        }),
+        knex('users').insert([
+          {
+            githubId: 3165635,
+            name: 'Olivier Tassinari',
+            login: 'oliviertassinari',
+            email: 'olivier.tassinari@gmail.com',
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+          },
+          {
+            githubId: 266302,
+            name: 'Greg Bergé',
+            login: 'neoziro',
+            email: 'berge.greg@gmail.com',
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+          },
+        ]),
       ])
     })
 }
