b19e8a43ba3eb494190aba292c896b67194acd6a
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-02-22 23:11:23 +0100

    fix(material-ui): using a link sounds better semantically speaking (#116)

--- src/review/routes/product/Signup.js
+++ src/review/routes/product/Signup.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Button from 'material-ui/Button'
-
-function handleClickSignup() {
-  window.location = '/auth/github'
-}
+import Link from 'modules/components/Link'
 
 export default function Signup() {
   return (
-    <Button raised accent onClick={handleClickSignup}>
+    <Button
+      raised
+      accent
+      component={Link}
+      href="/auth/github"
+    >
       {'Sign up'}
     </Button>
   )
