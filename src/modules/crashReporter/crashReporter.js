e02628a7c359a0e35cfb8445fd3c7b76eb754d63
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-02-26 13:22:07 +0100

    fix(sentry): disable in dev mode

--- src/modules/crashReporter/crashReporter.js
+++ src/modules/crashReporter/crashReporter.js
@@ -2,18 +2,26 @@
 
 let raven
 let DSN
-let config
+let ravenConfig
 
 if (process.env.PLATFORM === 'browser') {
   raven = require('raven-js')
-  DSN = 'https://f1690f74cc6e432e922f32da3eb051c9@sentry.io/133417'
-  config = {
+  if (process.env.NODE_ENV === 'production') {
+    DSN = 'https://f1690f74cc6e432e922f32da3eb051c9@sentry.io/133417'
+  } else {
+    DSN = false
+  }
+  ravenConfig = {
     release: window.clientData.releaseVersion,
   }
 } else {
   raven = require('raven')
-  DSN = 'https://261cb80891cb480fa452f7e18c0e57c0:dc050bb97a4d4692aa3e957c5c89d393@sentry.io/133418'
-  config = {
+  if (process.env.NODE_ENV === 'production') {
+    DSN = 'https://261cb80891cb480fa452f7e18c0e57c0:dc050bb97a4d4692aa3e957c5c89d393@sentry.io/133418'
+  } else {
+    DSN = false
+  }
+  ravenConfig = {
     autoBreadcrumbs: true,
     release: require('../../config').default.get('heroku.releaseVersion'),
   }
@@ -23,12 +31,9 @@ const crashReporter = {
   init: () => {
     raven.config(DSN, {
       environment: process.env.NODE_ENV, // Should always be production
-      ...config,
+      ...ravenConfig,
     })
-
-    if (process.env.NODE_ENV === 'production') {
-      raven.install()
-    }
+    raven.install()
   },
   captureException: (...args) => raven.captureException(...args),
   requestHandler: () => raven.requestHandler(),
