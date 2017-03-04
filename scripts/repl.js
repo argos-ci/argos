2344dc17d976629273160613408d84b3fd4707ea
Greg Bergé <greg@argos-ci.com>
2017-03-04 19:48:43 +0100

    chore: improve repl

--- scripts/repl.js
+++ scripts/repl.js
@@ -8,6 +8,7 @@ import { promirepl } from 'promirepl'
 import clearRequire from 'clear-require'
 
 const MODEL_DIRECTORY = path.join(__dirname, '../src/server/models')
+const JOB_DIRECTORY = path.join(__dirname, '../src/server/jobs')
 
 ;(async () => {
   const r = repl.start()
@@ -24,11 +25,24 @@ const MODEL_DIRECTORY = path.join(__dirname, '../src/server/models')
     }, {})
   }
 
+  async function getJobs() {
+    const jobs = await fs.readdir(JOB_DIRECTORY)
+    return jobs.reduce((jobs, job) => {
+      if (job.match(/\.test\.js$/)) {
+        return jobs
+      }
+
+      jobs[job.replace(/\.js$/, 'Job')] = require(path.join(JOB_DIRECTORY, job)).default
+      return jobs
+    }, {})
+  }
+
   const reload = async (log = true) => {
     if (log) console.log('Reloading...') // eslint-disable-line no-console
     clearRequire.match(new RegExp(MODEL_DIRECTORY))
     const models = await getModels()
-    Object.assign(r.context, models, { reload })
+    const jobs = await getJobs()
+    Object.assign(r.context, models, jobs, { reload })
     if (log) console.log('Reloaded!') // eslint-disable-line no-console
   }
 
