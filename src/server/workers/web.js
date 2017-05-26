5ae9c19c12e0b6e07e66faa26be16eb004d845c8
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-05-26 22:15:43 +0200

    feat: normalization v2 (#294)

--- src/server/workers/web.js
+++ src/server/workers/web.js
@@ -1,7 +1,4 @@
-/* eslint-disable no-console */
-
 import 'server/bootstrap/setup'
-
 import http from 'http'
 import config from 'config'
 import { addCloseCallback } from 'server/bootstrap/handleKillSignals'
@@ -9,7 +6,12 @@ import app from 'server/routes/app'
 
 const server = http.createServer(app)
 
-server.listen(config.get('server.port'), () => {
+server.listen(config.get('server.port'), (err) => {
+  if (err) {
+    throw err
+  }
+
+  // eslint-disable-next-line no-console
   console.log(`${Date(Date.now())}: http://localhost:${server.address().port}/`)
 })
 
