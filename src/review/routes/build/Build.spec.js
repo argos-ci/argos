a223cf9e3e3b1858ce634f2f1b89debfc8b958a2
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-05-30 20:38:28 +0200

    fix(review): hide the content when no right

--- src/review/routes/build/Build.spec.js
+++ src/review/routes/build/Build.spec.js
@@ -4,64 +4,62 @@ import BuildSummary from 'review/routes/build/BuildSummary'
 import Build from './Build'
 
 describe('<Build />', () => {
-  describe('task', () => {
-    let shallow
-    let fetch
-    let params
+  let shallow
+  let fetch
+  let params
 
-    beforeAll(() => {
-      shallow = createShallow({
-        otherContext: {
-          store: {
-            subscribe: () => {},
-            dispatch: () => {},
-            getState: () => ({
-              ui: {
-                build: {
-                  fetch,
-                },
-              },
-            }),
-          },
-        },
-      })
-      params = {
-        buildId: '1',
-      }
-    })
-
-    beforeEach(() => {
-      fetch = {
-        state: 'SUCCESS',
-        output: {
-          data: {
-            build: {
-              status: 'failure',
-              createdAt: null,
-              baseScreenshotBucket: {
-                commit: '',
-              },
-              compareScreenshotBucket: {
-                commit: '',
-                branch: null,
+  beforeAll(() => {
+    shallow = createShallow({
+      otherContext: {
+        store: {
+          subscribe: () => {},
+          dispatch: () => {},
+          getState: () => ({
+            ui: {
+              build: {
+                fetch,
               },
             },
-            screenshotDiffs: [],
-          },
+          }),
         },
-      }
+      },
     })
+    params = {
+      buildId: '1',
+    }
+  })
 
-    it('should render an error message', () => {
-      fetch.output.data.build = null
+  beforeEach(() => {
+    fetch = {
+      state: 'SUCCESS',
+      output: {
+        data: {
+          build: {
+            status: 'failure',
+            createdAt: null,
+            baseScreenshotBucket: {
+              commit: '',
+            },
+            compareScreenshotBucket: {
+              commit: '',
+              branch: null,
+            },
+          },
+          screenshotDiffs: [],
+        },
+      },
+    }
+  })
 
-      const wrapper = shallow(<Build params={params} />).until('Build').find('WatchTask').shallow()
-      expect(wrapper.debug()).toContain('Build not found.')
-    })
+  it('should render an error message', () => {
+    fetch.output.data.build = null
 
-    it('should render the children', () => {
-      const wrapper = shallow(<Build params={params} />).until('Build').find('WatchTask').shallow()
-      expect(wrapper.find(BuildSummary).length).toBe(1)
-    })
+    const wrapper = shallow(<Build params={params} />).until('Build').find('WatchTask').shallow()
+    expect(wrapper.debug()).toContain('Build not found.')
+  })
+
+  it('should render the children', () => {
+    const wrapper = shallow(<Build params={params} />).until('Build').find('WatchTask').shallow()
+    expect(wrapper.find(BuildSummary).length).toBe(1)
   })
 })
---
+++ src/review/routes/settings/Settings.spec.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import createShallow from 'material-ui/test-utils/createShallow'
+import Settings from './Settings'
+
+describe('<Settings />', () => {
+  let shallow
+  let fetch
+  let params
+
+  beforeAll(() => {
+    shallow = createShallow({
+      otherContext: {
+        store: {
+          subscribe: () => {},
+          dispatch: () => {},
+          getState: () => ({
+            ui: {
+              repository: {
+                fetch,
+              },
+            },
+          }),
+        },
+      },
+    })
+    params = {
+      profileName: 'callemall',
+      repositoryName: 'material-ui',
+    }
+  })
+
+  beforeEach(() => {
+    fetch = {
+      state: 'SUCCESS',
+      output: {
+        data: {
+          repository: {
+            authorization: false,
+            token: null,
+          },
+        },
+      },
+    }
+  })
+
+  it('should render an error message', () => {
+    const wrapper = shallow(<Settings params={params} />).until('Settings')
+    expect(wrapper.debug()).toContain('enough access right')
+  })
+})
