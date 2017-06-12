22a4705c2473005de6307db76ee721eb2899bbaa
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-06-12 18:53:44 +0200

    minor

--- src/modules/prism/index.js
+++ src/modules/prism/index.js
@@ -4,9 +4,8 @@ import 'prismjs/components/prism-clike'
 import 'prismjs/components/prism-javascript'
 import 'prismjs/components/prism-jsx'
 
-/* eslint-disable import/no-webpack-loader-syntax */
+// eslint-disable-next-line import/no-webpack-loader-syntax
 import lightTheme from '!raw-loader!prismjs/themes/prism.css'
-/* eslint-enable import/no-webpack-loader-syntax */
 
 const styleNode = window.document.createElement('style')
 styleNode.setAttribute('data-prism', true)
