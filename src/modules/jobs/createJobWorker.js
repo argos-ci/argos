028591383f5862e230ef89e4a3272a947982bdad
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-08-05 08:56:35 +0200

    core(js): upgrade node version

--- src/modules/jobs/createJobWorker.js
+++ src/modules/jobs/createJobWorker.js
@@ -1,9 +1,15 @@
+import display from 'modules/scripts/display'
 import { getChannel } from 'server/services/amqp'
 
 const createJobWorker = async (...jobs) => {
   try {
     const channel = await getChannel()
-    await Promise.all(jobs.map(job => job.process({ channel })))
+    await Promise.all(
+      jobs.map(job => {
+        display.info(`Start consuming ${job.queue} queue`)
+        return job.process({ channel })
+      })
+    )
   } catch (error) {
     setTimeout(() => {
       throw error
