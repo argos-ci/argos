c05efaa9b9e0d5a4a7da9aa45c431ec1e5f8f22b
Greg Bergé <greg@smooth-code.com>
2019-07-25 12:16:59 +0200

    chore: add @sentry/cli into dependencies

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,6 @@
 # Run migrations
 yarn api:db:migrate
 
-# Install sentry-cli
-yarn add @sentry/cli --dev
-
 # Setup releases on Sentry
 export SENTRY_AUTH_TOKEN=c1ca8a7da65a4482a8d7ea10c9e10dea9a5aff40d0a044cb8ae2d020f7291b8a
 export SENTRY_ORG=argos
