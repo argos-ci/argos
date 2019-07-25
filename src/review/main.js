cab423d4c76393d6836b1dbe199851f483af1dc3
Greg Bergé <greg@smooth-code.com>
2019-07-25 10:35:19 +0200

    chore: upgrade sentry

--- src/review/main.js
+++ src/review/main.js
@@ -6,10 +6,15 @@ import 'regenerator-runtime/runtime'
 import 'modules/rxjs'
 import React from 'react'
 import { render } from 'react-dom'
-import { initializeCrashReporterClient } from 'modules/crashReporter/client'
+import * as Sentry from '@sentry/browser'
+import configBrowser from 'configBrowser'
 import Root from 'review/Root'
 
-initializeCrashReporterClient()
+// Initialize Sentry
+Sentry.init({
+  dsn: configBrowser.get('sentry.clientDsn'),
+  release: configBrowser.get('releaseVersion'),
+})
 
 const renderRoot = () => {
   render(<Root />, document.querySelector('#root'))
