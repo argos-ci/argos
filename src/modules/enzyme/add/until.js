93b717256088852bfa40bfd21057db0632c19697
Vincent Audebert <vincent@smooth-code.com>
2019-10-30 13:02:48 +0100

    cleans tests

--- src/modules/enzyme/add/until.js
+++
@@ -1,4 +0,0 @@
-import ShallowWrapper from 'enzyme/ShallowWrapper'
-import until from '../operator/until'
-
-ShallowWrapper.prototype.until = until
--- src/server/bootstrap/setupTest.js
+++ src/server/bootstrap/setupTest.js
@@ -1,7 +1,4 @@
 /* eslint-disable no-console, no-underscore-dangle */
-
-// Add until method to enzyme Wrapper
-import 'modules/enzyme/add/until'
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import consoleError from './consoleError'
