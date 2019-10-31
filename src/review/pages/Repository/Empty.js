b6bbe14fb7552bb714845c23368beb1d734ccf3b
Vincent Audebert <vincent@smooth-code.com>
2019-10-31 11:43:10 +0100

    setup project card

--- src/review/pages/Repository/Empty.js
+++ src/review/pages/Repository/Empty.js
@@ -1,31 +1,19 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react'
-import styled, { Box, css } from '@xstyled/styled-components'
-import { up } from '@xstyled/system'
-import { Text, Button } from '@smooth-ui/core-sc'
+import styled from '@xstyled/styled-components'
 import {
   Container,
   Card,
   CardTitle,
   CardHeader,
   CardBody,
+  CardText,
   FadeLink,
 } from 'components'
-import Beast from 'components/Beast'
 import { useRepository } from './RepositoryContext'
 
-const StyledBeast = styled(Beast)`
-  position: relative;
-  width: 100rpx;
-  height: 80rpx;
-  fill: white;
-
-  ${up(
-    'md',
-    css`
-      margin-top: 20rpx;
-    `,
-  )}
+const StyledLink = styled(FadeLink)`
+  color: darker;
 `
 
 export function RepositoryEmpty() {
@@ -35,33 +23,18 @@ export function RepositoryEmpty() {
     <Container my={4}>
       <Card>
         <CardHeader>
-          <CardTitle>Waiting for screenshots...</CardTitle>
+          <CardTitle>Setup Argos on this project</CardTitle>
         </CardHeader>
         <CardBody>
-          <Box row mx={-3}>
-            <Box col={{ xs: 1, md: 1 / 3 }} px={3} textAlign="center">
-              <StyledBeast width={100} />
-            </Box>
-            <Box col={{ xs: 1, md: 2 / 3 }} px={3}>
-              <Text as="p" mb={2}>
-                Our screenshot beast is waiting to scan your first screenshots.
-              </Text>
-              <p>
-                <Button
-                  as="a"
-                  href={`/${repository.owner.login}/${repository.name}/getting-started`}
-                >
-                  Installation Instructions
-                </Button>
-              </p>
-              <FadeLink
-                href={`/${repository.owner.login}/${repository.name}/builds/${repository.sampleBuildId}?sample`}
-                color="white"
-              >
-                Or see a sample build
-              </FadeLink>
-            </Box>
-          </Box>
+          <CardText>
+            See{' '}
+            <StyledLink
+              href={`/${repository.owner.login}/${repository.name}/getting-started`}
+            >
+              Quick Start Guide
+            </StyledLink>{' '}
+            to learn how to setup the project.
+          </CardText>
         </CardBody>
       </Card>
     </Container>
