13323ee61118623ea293f8dd98506be586decd42
Greg Bergé <greg@smooth-code.com>
2020-03-09 09:17:19 +0100

    chore: add a way to disable Sentry releases

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,15 +1,20 @@
+set -e
+
 # Run migrations
 yarn api:db:migrate
 
-# Setup releases on Sentry
-export SENTRY_ORG=argos
-VERSION=$HEROKU_SLUG_COMMIT	
+if [ "$SENTRY_RELEASE_DISABLED" != "true" ]; then
+  # Setup releases on Sentry
+  export SENTRY_ORG=argos
+  VERSION=$HEROKU_RELEASE_VERSION	
 
-# Create a release
-yarn sentry-cli releases new -p argos-browser -p argos-server $VERSION
+  # Create a release
+  yarn sentry-cli releases new -p argos-browser -p argos-server $VERSION
 
-# Associate commits with the release
-yarn sentry-cli releases set-commits --commit "argos-ci/argos@$VERSION" $VERSION
+  # Associate commits with the release
+  yarn sentry-cli releases set-commits --commit "argos-ci/argos@$VERSION" $VERSION
 
-# Mark the deploy
-sentry-cli releases deploys $VERSION new -e production
+  # Mark the deploy
+  sentry-cli releases deploys $VERSION new -e production
+
+fi
