a7bc8b1575afbd8dee56aaf66898831cb0bb7ae0
afc163 <afc163@gmail.com>
2022-04-11 12:20:51 +0800

    style: Add loading status to mark button

--- apps/app/src/pages/Repository/BuildDetail/Action.js
+++ apps/app/src/pages/Repository/BuildDetail/Action.js
@@ -35,6 +35,7 @@ export default function BuildDetailAction({ build }) {
   return (
     <Button
       disabled={loading}
+      loading={loading}
       variant={variant}
       onClick={() =>
         setValidationStatus({
