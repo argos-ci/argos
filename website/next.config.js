7a049592b5d9e009e7f69870ab4918e40e467887
Jeremy Sfez <jeremy@smooth-code.com>
2022-07-31 12:59:03 +0200

    feat: add build redirection

--- website/next.config.js
+++ website/next.config.js
@@ -6,10 +6,24 @@ const withMDX = require("@next/mdx")({
     providerImportSource: "@mdx-js/react",
   },
 });
-module.exports = withMDX({
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+
+module.exports = {
+  ...withMDX({
+    i18n: {
+      locales: ["en"],
+      defaultLocale: "en",
+    },
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  }),
+
+  async redirects() {
+    return [
+      {
+        source: "/:organization/:repository/builds/:path*",
+        destination:
+          "https://app.argos-ci.com/:organization/:repository/builds/:path*",
+        permanent: true,
+      },
+    ];
   },
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-});
+};
