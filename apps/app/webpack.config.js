f879195bc91fce8758b09adaf1f8109bf05202ce
Greg Bergé <berge.greg@gmail.com>
2022-08-03 15:08:37 +0200

    chore: fix webpack config

--- apps/app/webpack.config.js
+++ apps/app/webpack.config.js
@@ -68,6 +68,10 @@ module.exports = {
         test: /\.html$/,
         loader: 'html-loader',
       },
+      {
+        test: /\.md$/,
+        loader: 'raw-loader',
+      },
     ],
   },
   plugins: [
