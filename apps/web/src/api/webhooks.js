4a4778c8d8243da3e465042e5d1021b280354c2b
Greg Bergé <berge.greg@gmail.com>
2022-08-09 22:14:06 +0200

    chore: upgrade octokit/webhooks

--- apps/web/src/api/webhooks.js
+++ apps/web/src/api/webhooks.js
@@ -1,14 +1,15 @@
-import WebhooksApi from "@octokit/webhooks";
+import { Webhooks, createNodeMiddleware } from "@octokit/webhooks";
 import config from "@argos-ci/config";
 import { handleGitHubEvents } from "@argos-ci/synchronize";
 
-const webhooks = new WebhooksApi({
+const webhooks = new Webhooks({
   secret: config.get("github.webhookSecret"),
-  path: "/github/event-handler",
 });
 
-webhooks.on("*", (...args) => {
+webhooks.onAny((...args) => {
   handleGitHubEvents(...args);
 });
 
-export default webhooks.middleware;
+export default createNodeMiddleware(webhooks, {
+  path: "/github/event-handler",
+});
