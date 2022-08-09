84e46f786d28e650da1683f3cc5d1b5edf59f136
Jeremy Sfez <jeremy@smooth-code.com>
2022-08-09 19:18:08 +0200

    test: pending_change_cancelled event

---
+++ apps/synchronize/src/github/cancelChangeEvent.test.js
@@ -0,0 +1,63 @@
+import { useDatabase, factory } from '@argos-ci/database/testing'
+import { Purchase } from '@argos-ci/database/models'
+import { handleGitHubEvents } from './events'
+import { CHANGE_EVENT_PAYLOAD } from '../fixtures/change-event-payload'
+
+describe('marketplace "pending_change_cancelled" event', () => {
+  useDatabase()
+
+  const pendingChangeCancelPayload = {
+    ...CHANGE_EVENT_PAYLOAD,
+    action: 'pending_change_cancelled',
+  }
+
+  describe('on registered purchase', () => {
+    let account
+    let plan
+
+    beforeEach(async () => {
+      const organization = await factory.create('Organization', {
+        githubId:
+          pendingChangeCancelPayload.previous_marketplace_purchase.account.id,
+      })
+      account = await factory.create('Account', {
+        organizationId: organization.id,
+        userId: null,
+      })
+      plan = await factory.create('Plan', {
+        githubId:
+          pendingChangeCancelPayload.previous_marketplace_purchase.plan.id,
+      })
+      await factory.create('Purchase', {
+        accountId: account.id,
+        planId: plan.id,
+      })
+      await handleGitHubEvents({
+        name: 'marketplace_purchase',
+        payload: pendingChangeCancelPayload,
+      })
+    })
+
+    it('should remove end date', async () => {
+      const purchase = await Purchase.query().findOne({
+        accountId: account.id,
+        planId: plan.id,
+      })
+      expect(purchase.endDate).toBeNull()
+    })
+  })
+
+  describe('on missing purchase', () => {
+    it('should throw an error', async () => {
+      expect.assertions(1)
+      try {
+        await handleGitHubEvents({
+          name: 'marketplace_purchase',
+          payload: pendingChangeCancelPayload,
+        })
+      } catch (error) {
+        expect(error.message).toMatch('missing purchase')
+      }
+    })
+  })
+})
