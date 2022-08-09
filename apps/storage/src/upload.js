6f1582956237dd5b73c37f1878cf552dae911e14
Greg Bergé <berge.greg@gmail.com>
2022-08-09 23:46:43 +0200

    chore: upgrade aws

--- apps/storage/src/upload.js
+++ apps/storage/src/upload.js
@@ -1,14 +1,17 @@
 import { createReadStream } from "fs";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { randomUUID } from "crypto";
 import mime from "mime";
 
-export function upload({ s3, inputPath, ...other }) {
-  return s3
-    .upload({
+export async function upload({ s3, inputPath, Key: KeyArg, ...other }) {
+  const Key = KeyArg || randomUUID();
+  await s3.send(
+    new PutObjectCommand({
       Body: createReadStream(inputPath),
       ContentType: mime.getType(inputPath),
-      Key: randomUUID(),
+      Key,
       ...other,
     })
-    .promise();
+  );
+  return { Key };
 }
