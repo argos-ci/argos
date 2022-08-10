1c7ab8ab71b58f896465d1a043b5091830430e51
Greg Bergé <berge.greg@gmail.com>
2022-08-10 08:09:48 +0200

    chore: use npm

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Run migrations
-yarn workspace @argos-ci/database db:migrate:latest
+npm run -w @argos-ci/database db:migrate:latest
 
 if [ "$SENTRY_RELEASE_DISABLED" != "true" ]; then
   # Setup releases on Sentry
@@ -11,14 +11,14 @@ if [ "$SENTRY_RELEASE_DISABLED" != "true" ]; then
   export SENTRY_ORG=argos
 
   # Create a release
-  yarn sentry-cli releases new -p argos-browser -p argos-server $VERSION	
+  npx @sentry/cli releases new -p argos-browser -p argos-server $VERSION	
 
   # Associate commits with the release
-  yarn sentry-cli releases set-commits $VERSION --commit "argos-ci/argos@$VERSION" 
+  npx @sentry/cli releases set-commits $VERSION --commit "argos-ci/argos@$VERSION" 
 
   # Finalize the version
-  yarn sentry-cli releases finalize $VERSION
+  npx @sentry/cli releases finalize $VERSION
 
   # Mark the deploy
-  yarn sentry-cli releases deploys $VERSION new -e production
+  npx @sentry/cli releases deploys $VERSION new -e production
 fi
