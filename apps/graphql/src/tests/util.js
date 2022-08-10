007d10a7ede55d2fe32bdff05d0647a5dae9618c
Greg Bergé <berge.greg@gmail.com>
2022-08-10 09:54:00 +0200

    chore: fix test apollo server multi start

--- apps/graphql/src/tests/util.js
+++ apps/graphql/src/tests/util.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+let started = false;
+
 export const createApolloServerApp = async (apolloServer, { user } = {}) => {
   const app = express();
   app.use((req, res, next) => {
@@ -7,7 +9,10 @@ export const createApolloServerApp = async (apolloServer, { user } = {}) => {
     next();
   });
 
-  await apolloServer.start();
+  if (!started) {
+    await apolloServer.start();
+  }
+  started = true;
   apolloServer.applyMiddleware({ app });
 
   return app;
