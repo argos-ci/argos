8da307672a767c47a39c2675c3126f30c5fe4a90
Greg Bergé <berge.greg@gmail.com>
2022-08-26 16:34:01 +0200

    fix(link): fix forward ref

--- apps/app/src/components/Link.js
+++ apps/app/src/components/Link.js
@@ -2,28 +2,33 @@ import * as React from "react";
 import { x } from "@xstyled/styled-components";
 import { Link as ReactRouterLink } from "react-router-dom";
 
-export const BaseLink = (props) => (
-  <x.a
-    transition="base"
-    as={props.to ? ReactRouterLink : "a"}
-    textDecoration={{ _: "none", hover: "none" }}
-    {...(props.target === "_blank" ? { rel: "noopener noreferrer" } : {})}
-    {...props}
-  />
-);
+export const BaseLink = React.forwardRef((props, ref) => {
+  return (
+    <x.a
+      ref={ref}
+      transition="base"
+      as={props.to ? ReactRouterLink : "a"}
+      textDecoration={{ _: "none", hover: "none" }}
+      {...(props.target === "_blank" ? { rel: "noopener noreferrer" } : {})}
+      {...props}
+    />
+  );
+});
 
-export const Link = ({ ...props }) => (
+export const Link = React.forwardRef((props, ref) => (
   <BaseLink
+    ref={ref}
     textDecoration={{ _: "none", hover: "underline", focus: "underline" }}
     color="link"
     cursor="pointer"
     {...props}
   />
-);
+));
 
-export function LinkBlock(props) {
+export const LinkBlock = React.forwardRef((props, ref) => {
   return (
     <BaseLink
+      ref={ref}
       borderRadius="md"
       backgroundColor={{
         _: "inherit",
@@ -33,4 +38,4 @@ export function LinkBlock(props) {
       {...props}
     />
   );
-}
+});
