2f17ae26e688ea22b1ceaa500a7a4bde2db57ac4
Jeremy SFEZ <jeremy@smooth-code.com>
2022-09-05 17:01:53 +0200

    Update apps/app/src/components/Checkbox.js
    Co-authored-by: Greg Bergé <berge.greg@gmail.com>

--- apps/app/src/components/Checkbox.js
+++ apps/app/src/components/Checkbox.js
@@ -2,4 +2,4 @@ import * as React from "react";
 import { x } from "@xstyled/styled-components";
 import { Checkbox as AriakitCheckbox } from "ariakit/checkbox";
 
-export const Checkbox = (props) => <x.div as={AriakitCheckbox} {...props} />;
+export const Checkbox = (props) => <x.checkbox as={AriakitCheckbox} {...props} />;
