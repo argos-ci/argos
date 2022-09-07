03511e6b250873c5fe6c99cafca41850120f5ca1
Greg Bergé <berge.greg@gmail.com>
2022-09-07 23:10:02 +0200

    fix(build): fix build display

--- apps/app/src/pages/Build/ScreenshotsDiffCard.js
+++ apps/app/src/pages/Build/ScreenshotsDiffCard.js
@@ -94,7 +94,7 @@ export function ScreenshotsDiffCard({
             as={ScreenshotDiffStatusIcon(screenshotDiff.status)}
             color={getStatusPrimaryColor(screenshotDiff.status)}
           />
-          {baseScreenshot.name}
+          {compareScreenshot.name || baseScreenshot.name}
         </CardTitle>
       </CardHeader>
 
