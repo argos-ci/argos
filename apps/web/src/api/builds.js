9aa23f5b7fa52b5458854b63a00c3b022df09db6
Greg Bergé <berge.greg@gmail.com>
2022-09-07 12:18:16 +0200

    fix: avoid returning 500

--- apps/web/src/api/builds.js
+++ apps/web/src/api/builds.js
@@ -1,14 +1,16 @@
 import { HttpError } from "express-err";
 import express from "express";
+import { asyncHandler } from "../util";
 
 const router = express.Router();
 export default router;
 
-router.post("/builds", (req, res, next) => {
-  next(
-    new HttpError(
+router.post(
+  "/builds",
+  asyncHandler(() => {
+    throw new HttpError(
       400,
       "argos-cli is deprecated, use @argos-ci/cli instead. Check https://docs.argos-ci.com"
-    )
-  );
-});
+    );
+  })
+);
