c391579f63de1644d8bc00155a3c021ccd668a75
Greg Bergé <berge.greg@gmail.com>
2022-10-08 11:57:57 +0200

    test(e2e): responsive + no sticky header

--- website/cypress/e2e/pages.cy.js
+++ website/cypress/e2e/pages.cy.js
@@ -5,18 +5,39 @@ const pages = {
   security: "/security",
 };
 
-Object.keys(pages).forEach((pageName) => {
-  describe(`Page: ${pages[pageName]}`, () => {
+const viewportPresets = ["macbook-16", "ipad-2", "iphone-8"];
+
+function injectStyles(document, styles) {
+  const css = document.createElement("style");
+  css.type = "text/css";
+  css.textContent = styles;
+  document.body.appendChild(css);
+}
+
+viewportPresets.forEach((viewportPreset) => {
+  describe(viewportPreset, () => {
     beforeEach(() => {
-      cy.visit(pages[pageName]);
+      cy.viewport(viewportPreset);
     });
+    Object.keys(pages).forEach((pageName) => {
+      const name = `${viewportPreset}/${pageName}`;
 
-    it(`should screenshot`, () => {
-      cy.argosScreenshot(pageName);
-    });
-
-    it(`should check url`, () => {
-      cy.url().should("match", /^http:\/\/localhost/);
+      it(pageName, () => {
+        cy.visit(pages[pageName]);
+        cy.wait(200);
+        cy.document().then((document) => {
+          injectStyles(
+            document,
+            `
+            /* No sticky header */
+            nav {
+              position: initial !important;
+            }
+          `
+          );
+        });
+        cy.argosScreenshot(name);
+      });
     });
   });
 });
