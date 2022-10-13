5050ad6fb12b2c023b50c894d949e287dd2c95ac
Greg Bergé <berge.greg@gmail.com>
2022-10-13 16:53:31 +0200

    chore: allow to process 2 diffs in parallel

--- apps/job-core/src/createModelJob.js
+++ apps/job-core/src/createModelJob.js
@@ -1,22 +1,26 @@
 import { createJob } from "./createJob";
 
-export function createModelJob(queue, Model, perform) {
-  return createJob(queue, {
-    async perform(id) {
-      const model = await Model.query().findById(id);
+export function createModelJob(queue, Model, perform, params) {
+  return createJob(
+    queue,
+    {
+      async perform(id) {
+        const model = await Model.query().findById(id);
 
-      if (!model) {
-        throw new Error(`${Model.name} not found`);
-      }
+        if (!model) {
+          throw new Error(`${Model.name} not found`);
+        }
 
-      await model.$query().patch({ jobStatus: "progress" }).returning("*");
-      await perform(model);
+        await model.$query().patch({ jobStatus: "progress" }).returning("*");
+        await perform(model);
+      },
+      async error(id) {
+        await Model.query().patch({ jobStatus: "error" }).where({ id });
+      },
+      async complete(id) {
+        await Model.query().patch({ jobStatus: "complete" }).where({ id });
+      },
     },
-    async error(id) {
-      await Model.query().patch({ jobStatus: "error" }).where({ id });
-    },
-    async complete(id) {
-      await Model.query().patch({ jobStatus: "complete" }).where({ id });
-    },
-  });
+    params
+  );
 }
