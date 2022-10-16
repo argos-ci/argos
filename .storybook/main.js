2b83f0f2f46dfbb21d8a6f4a95dfd0293b2f50f6
Jeremy Sfez <jeremy@smooth-code.com>
2022-10-16 23:09:18 +0200

    test: setup storybook

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+const path = require("path");
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../apps"),
+    };
+
+    return config;
+  },
+};
