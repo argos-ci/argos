4b5d762871a5a71054ee750fd2eeee336ec86a25
Greg Bergé <berge.greg@gmail.com>
2022-11-22 12:09:31 +0100

    chore: reactivate sentry

--- apps/sentry-node/src/index.ts
+++ apps/sentry-node/src/index.ts
@@ -1,12 +1,12 @@
 import { init } from "@sentry/node";
 
-// import config from "@argos-ci/config";
+import config from "@argos-ci/config";
 
 export function setup() {
   init({
-    // dsn: config.get("sentry.serverDsn"),
-    // environment: config.get("sentry.environment"),
-    // release: config.get("releaseVersion"),
+    dsn: config.get("sentry.serverDsn"),
+    environment: config.get("sentry.environment"),
+    release: config.get("releaseVersion"),
   });
 }
 
