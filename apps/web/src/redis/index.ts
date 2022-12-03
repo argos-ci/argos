fdf1fc6a55d3b881fbba0205a8d80cc12c56f35e
Greg Bergé <berge.greg@gmail.com>
2022-12-03 15:00:27 +0100

    fix(redis): fix socket closed
    Fix ARGOS-SERVER-XFV

--- apps/web/src/redis/index.ts
+++ apps/web/src/redis/index.ts
@@ -2,10 +2,24 @@ import { callbackify } from "node:util";
 import { createClient } from "redis";
 
 import config from "@argos-ci/config";
+import logger from "@argos-ci/logger";
 
 import { createRedisLock } from "./lock.js";
 
 const redisClient = createClient({ url: config.get("redis.url") });
+redisClient.on("error", (err: unknown) => {
+  logger.error(err);
+});
+redisClient.on("connect", () => {
+  logger.info("Redis client is connected");
+});
+redisClient.on("reconnecting", () => {
+  logger.info("Redis client is reconnecting");
+});
+redisClient.on("ready", () => {
+  logger.info("Redis client is ready");
+});
+
 const redisLock = createRedisLock(redisClient);
 
 let connection: Promise<void> | null = null;
