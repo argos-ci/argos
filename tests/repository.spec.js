de62f06e5852fff1cbfe7fa7d2594c485bbfd734
Greg Bergé <berge.greg@gmail.com>
2022-12-04 13:44:48 +0100

    chore: fix E2E

--- tests/repository.spec.js
+++ tests/repository.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require("@playwright/test");
 const { goto, argosScreenshot } = require("./utils");
 
 test("build list", async ({ page, browserName }) => {
-  await goto({ page, link: "/callemall/material-ui/builds" });
+  await goto({ page, link: "/callemall/material-ui" });
   await expect(page.getByText("#12")).toBeVisible();
   await argosScreenshot(page, `owner-repository-list-${browserName}`);
 });
