4e51f0052c3550502ab76387e9beaa2ade47f4e3
Jeremy Sfez <jeremy@smooth-code.com>
2022-12-21 09:19:49 +0100

    test: take blog screenshots

--- website/cypress/e2e/pages.cy.js
+++ website/cypress/e2e/pages.cy.js
@@ -3,6 +3,9 @@ const pages = {
   terms: "/terms",
   privacy: "/privacy",
   security: "/security",
+  blog: "/blog",
+  "blog-post-1": "/blog/visual-testing",
+  "blog-post-2": "/blog/improve-dx",
 };
 
 const viewportPresets = ["macbook-16", "ipad-2", "iphone-8"];
