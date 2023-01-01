bbccc9d92bb2b198137101635144b7bd0eba2201
Greg Bergé <berge.greg@gmail.com>
2023-01-01 10:19:57 +0100

    refactor: rename S3Image -> ImageFile

--- apps/storage/src/index.ts
+++ apps/storage/src/index.ts
@@ -4,5 +4,5 @@ export * from "./get.js";
 export * from "./getPublicUrl.js";
 export * from "./upload.js";
 export * from "./signed-url.js";
+export * from "./ImageFile.js";
 export * from "./s3.js";
-export * from "./S3Image.js";
