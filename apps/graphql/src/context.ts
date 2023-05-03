b1d0ac258df0f08eb99a9e67945ec05aa3e0f70b
Greg Bergé <berge.greg@gmail.com>
2023-05-03 22:09:42 +0200

    chore(apollo): upgrade apollo server

--- apps/graphql/src/context.ts
+++ apps/graphql/src/context.ts
@@ -1,11 +1,12 @@
+import type { BaseContext } from "@apollo/server";
 import type { Request } from "express";
 
 import { createLoaders } from "./loaders.js";
 
-export interface Context {
+export type Context = BaseContext & {
   auth: Request["auth"];
   loaders: ReturnType<typeof createLoaders>;
-}
+};
 
 export const getContext = ({ req }: { req: Request }): Context => ({
   auth: req.auth,
