a230cf06c2677c5d7e46fe262c571ece0f166325
Greg Bergé <berge.greg@gmail.com>
2023-11-25 00:14:19 +0100

    chore(email): tolerate no resend api key in dev & test

--- apps/backend/src/email/send.ts
+++ apps/backend/src/email/send.ts
@@ -2,12 +2,18 @@ import { WelcomeEmail } from "./welcome.js";
 import { Resend } from "resend";
 import config from "@/config/index.js";
 
-const resend = new Resend(config.get("resend.apiKey"));
+const production = config.get("env") === "production";
+const resendApiKey = config.get("resend.apiKey");
+
+// Resend throws if API key is missing
+// It is tolerable in test and development but not in production
+const resend =
+  resendApiKey || production ? new Resend(config.get("resend.apiKey")) : null;
 
 const from = "Argos <contact@argos-ci.com>";
 
 export async function sendWelcomeEmail({ to }: { to: string }) {
-  await resend.emails.send({
+  await resend?.emails.send({
     from,
     to: [to],
     subject: "Welcome to Argos!",
