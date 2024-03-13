5ecb63bc16f8700b6a4c386b4c25ef0795d3b99f
Greg Bergé <berge.greg@gmail.com>
2024-03-13 16:46:33 +0100

    chore: remove sentry release code

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,21 +4,3 @@ set -e
 
 # Run migrations
 npm run -w @argos/backend db:migrate:latest
-
-if [ "$SENTRY_RELEASE_DISABLED" != "true" ]; then
-  # Setup releases on Sentry
-  VERSION=$HEROKU_SLUG_COMMIT
-  export SENTRY_ORG=argos
-
-  # Create a release
-  npx @sentry/cli releases new -p argos-browser -p argos-server $VERSION
-
-  # Associate commits with the release
-  npx @sentry/cli releases set-commits $VERSION --commit "argos-ci/argos@$VERSION"
-
-  # Finalize the version
-  npx @sentry/cli releases finalize $VERSION
-
-  # Mark the deploy
-  npx @sentry/cli releases deploys $VERSION new -e production
-fi
