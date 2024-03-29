9f1f22decfc980e86c4f8908ad7477af407100cc
Greg Bergé <berge.greg@gmail.com>
2024-03-29 21:35:55 +0100

    chore: use SSL only in prod

--- apps/backend/src/config/index.ts
+++ apps/backend/src/config/index.ts
@@ -332,7 +332,9 @@ if (process.env["DATABASE_URL"]) {
   config.set("pg.connection.user", url.username);
   config.set("pg.connection.password", url.password);
   config.set("pg.connection.database", url.pathname.substring(1));
-  config.set("pg.connection.ssl", { rejectUnauthorized: false });
+  if (env === "production") {
+    config.set("pg.connection.ssl", { rejectUnauthorized: false });
+  }
   config.set("pg.connection.timezone", "utc");
 }
 
