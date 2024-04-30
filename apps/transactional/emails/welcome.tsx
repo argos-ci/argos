62940caf4ae3f79198258bcbd273ee8d73a3acc1
Greg Bergé <berge.greg@gmail.com>
2024-05-01 00:46:20 +0200

    chore: slim slug

--- apps/transactional/emails/welcome.tsx
+++
@@ -1,7 +0,0 @@
-import { WelcomeEmail } from "../../backend/src/email/welcome.js";
-
-function ArgosWelcomeExample() {
-  return <WelcomeEmail baseUrl="https://app.argos-ci.dev:4002" />;
-}
-
-export default ArgosWelcomeExample;
