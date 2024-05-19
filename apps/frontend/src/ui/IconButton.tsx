e299b1b9f6854cf3f3af17f3f1f7b6f65b26aabc
Greg Bergé <berge.greg@gmail.com>
2024-05-19 09:45:01 +0200

    fix(IconButton): fix disabled hover / active interactions

--- apps/frontend/src/ui/IconButton.tsx
+++ apps/frontend/src/ui/IconButton.tsx
@@ -11,17 +11,17 @@ const colorClassNames: Record<
 > = {
   contained: {
     neutral:
-      "hover:border-hover hover:bg-ui text-low hover:text bg-ui/60 focus-visible:ring-default",
+      "[&:not([aria-disabled])]:hover:border-hover [&:not([aria-disabled])]:hover:bg-ui text-low [&:not([aria-disabled])]:hover:text bg-ui/60 focus-visible:ring-default",
     danger: "", // not used
     success: "", // not used
   },
   outline: {
     neutral:
-      "hover:border-hover text-low aria-pressed:bg-active aria-pressed:text active:bg-active active:text focus-visible:ring-default",
+      "[&:not([aria-disabled])]:hover:border-hover text-low aria-pressed:bg-active aria-pressed:text [&:not([aria-disabled])]:active:bg-active [&:not([aria-disabled])]:active:text focus-visible:ring-default",
     danger:
-      "hover:border-danger-hover text-danger-low aria-pressed:bg-danger-active active:bg-danger-active focus-visible:ring-danger",
+      "[&:not([aria-disabled])]:hover:border-danger-hover text-danger-low aria-pressed:bg-danger-active [&:not([aria-disabled])]:active:bg-danger-active focus-visible:ring-danger",
     success:
-      "hover:border-success-hover text-success-low aria-pressed:bg-success-active active:bg-success-active focus-visible:ring-success",
+      "[&:not([aria-disabled])]:hover:border-success-hover text-success-low aria-pressed:bg-success-active [&:not([aria-disabled])]:active:bg-success-active focus-visible:ring-success",
   },
 };
 
@@ -35,7 +35,15 @@ export const IconButton = forwardRef<
   }
 >(
   (
-    { color = "neutral", variant = "outline", children, asChild, ...props },
+    {
+      color = "neutral",
+      variant = "outline",
+      children,
+      asChild,
+      onClick,
+      disabled,
+      ...props
+    },
     ref,
   ) => {
     const variantClassName = colorClassNames[variant][color];
@@ -43,15 +51,23 @@ export const IconButton = forwardRef<
     return (
       <Comp
         ref={ref}
+        aria-disabled={disabled ? "true" : undefined}
         className={clsx(
           variantClassName,
           /* Group */
           "group-[]/button-group:rounded-none group-[]/button-group:first:rounded-l-lg group-[]/button-group:last:rounded-r-lg",
           /* Base */
-          "disabled:opacity-disabled flex h-8 cursor-default items-center gap-2 rounded-lg border border-transparent p-[7px] text-sm transition [&>*]:size-4",
+          "aria-[disabled]:opacity-disabled flex h-8 cursor-default items-center gap-2 rounded-lg border border-transparent p-[7px] text-sm transition [&>*]:size-4",
           /* Focus */
           "focus:outline-none focus-visible:ring-4",
         )}
+        onClick={(event) => {
+          if (props["aria-disabled"] || disabled) {
+            event.preventDefault();
+            return;
+          }
+          onClick?.(event);
+        }}
         {...props}
       >
         {children}
