2a783d3141035df59a63c29879dba81dd9cab4d6
Greg Bergé <berge.greg@gmail.com>
2024-06-04 09:13:50 +0200

    feat: use pixelated when scale > 1.5

--- apps/frontend/src/ui/TwicPicture.tsx
+++ apps/frontend/src/ui/TwicPicture.tsx
@@ -1,13 +1,18 @@
-export function TwicPicture(
+import { forwardRef } from "react";
+
+export const TwicPicture = forwardRef(function TwicPicture(
   props: React.ImgHTMLAttributes<HTMLImageElement> & {
     src: string;
     transforms?: string[];
     original?: boolean;
   },
+  ref: React.Ref<HTMLImageElement>,
 ) {
   const { src, transforms = [], original, ...rest } = props;
   if (original) {
-    return <img src={twic(src, ["format=png", ...transforms])} {...rest} />;
+    return (
+      <img ref={ref} src={twic(src, ["format=png", ...transforms])} {...rest} />
+    );
   }
   return (
     <picture>
@@ -24,12 +29,13 @@ export function TwicPicture(
         type="image/png"
       />
       <img
+        ref={ref}
         src={twic(src, ["format=png", "quality=90", ...transforms])}
         {...rest}
       />
     </picture>
   );
-}
+});
 
 function twic(url: string, transforms: string[]): string {
   return `${url}?twic=v1/${transforms.join("/")}`;
