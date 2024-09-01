44537eee5be8627f4817a75b531ed3bf0bd8918f
Greg Bergé <berge.greg@gmail.com>
2024-09-01 11:25:09 +0200

    fix: comment support multiple projects linked to the same repo

--- apps/backend/src/build-notification/comment.ts
+++ apps/backend/src/build-notification/comment.ts
@@ -3,45 +3,79 @@ import { invariant } from "@argos/util/invariant";
 import { getBuildLabel } from "@/build/label.js";
 import { getStatsMessage } from "@/build/stats.js";
 import { getCommentHeader } from "@/database/index.js";
-import { Build } from "@/database/models/index.js";
+import { Build, Project } from "@/database/models/index.js";
 
 const dateFormatter = Intl.DateTimeFormat("en-US", {
   dateStyle: "medium",
   timeStyle: "short",
   timeZone: "UTC",
 });
-const formatDate = (date: string): string => {
+
+function formatDate(date: string): string {
   const d = new Date(date);
   return dateFormatter.format(d);
-};
+}
+
+/**
+ * Get the name of the build.
+ * If the build is associated with a project, the project name is prepended.
+ */
+function getBuildName(input: { build: Build; project: Project | null }) {
+  if (input.project) {
+    return `${input.project.name}/${input.build.name}`;
+  }
+
+  return input.build.name;
+}
 
 export const getCommentBody = async (props: {
   commit: string;
 }): Promise<string> => {
   const builds = await Build.query()
-    .distinctOn("builds.name")
+    .distinctOn("builds.name", "builds.projectId")
     .joinRelated("compareScreenshotBucket")
+    .withGraphFetched("project")
     .where("compareScreenshotBucket.commit", props.commit)
     .orderBy([
-      { column: "name", order: "desc" },
+      { column: "builds.name", order: "desc" },
+      { column: "builds.projectId", order: "desc" },
       { column: "builds.number", order: "desc" },
     ]);
 
+  const hasMultipleProjects =
+    new Set(builds.map((build) => build.projectId)).size > 1;
   const aggregateStatuses = await Build.getAggregatedBuildStatuses(builds);
   const buildRows = await Promise.all(
-    builds.map(async (build, index) => {
-      const [stats, url] = await Promise.all([
-        getStatsMessage(build.id),
-        build.getUrl(),
-      ]);
-      const status = aggregateStatuses[index];
-      invariant(status, "unknown build status");
-      const label = getBuildLabel(build.type, status);
-      const review = status === "diffDetected" ? ` ([Review](${url}))` : "";
-      return `| **${build.name}** ([Inspect](${url})) | ${label}${review} | ${
-        stats || "-"
-      } | ${formatDate(build.updatedAt)} |`;
-    }),
+    builds
+      // Filter out builds that are not associated with a project that has PR comments enabled.
+      // We don't filter at SQL level because we still want to know if it's linked to multiple projects
+      // to determine if we should prepend the project name to the build name.
+      .filter((build) => {
+        invariant(build.project, "Relation `project` should be fetched");
+        return build.project.prCommentEnabled;
+      })
+      .map(async (build, index) => {
+        invariant(build.project, "Relation `project` should be fetched");
+
+        const [stats, url] = await Promise.all([
+          getStatsMessage(build.id),
+          build.getUrl(),
+        ]);
+
+        const status = aggregateStatuses[index];
+        invariant(status, "unknown build status");
+
+        const label = getBuildLabel(build.type, status);
+        const review = status === "diffDetected" ? ` ([Review](${url}))` : "";
+        const name = getBuildName({
+          build,
+          project: hasMultipleProjects ? build.project : null,
+        });
+
+        return `| **${name}** ([Inspect](${url})) | ${label}${review} | ${
+          stats || "-"
+        } | ${formatDate(build.updatedAt)} |`;
+      }),
   );
   return [
     getCommentHeader(),
