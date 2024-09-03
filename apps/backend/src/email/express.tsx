4da2e35bdc76f6b4c653b6c91b824b9a19851aa7
Greg Bergé <berge.greg@gmail.com>
2024-09-03 09:04:47 +0200

    chore: fix email preview

--- apps/backend/src/email/express.tsx
+++ apps/backend/src/email/express.tsx
@@ -1,15 +1,18 @@
 import { render } from "@react-email/render";
 import { Router } from "express";
 
-import config from "@/config/index.js";
+import { asyncHandler } from "@/web/util.js";
 
 import { WelcomeEmail } from "./welcome.js";
 
 const router = Router();
 
-router.get("/welcome", (_req, res) => {
-  const html = render(<WelcomeEmail baseUrl={config.get("server.url")} />);
-  res.send(html);
-});
+router.get(
+  "/welcome",
+  asyncHandler(async (_req, res) => {
+    const html = await render(<WelcomeEmail baseUrl="/" />);
+    res.send(html);
+  }),
+);
 
 export { router as emailPreview };
