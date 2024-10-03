c1900a1fcca1cad81e36ec46d7874c2df6db299b
Greg Bergé <berge.greg@gmail.com>
2024-10-03 20:16:55 +0200

    chore: fix react-router typing

--- apps/frontend/src/router.tsx
+++ apps/frontend/src/router.tsx
@@ -2,10 +2,10 @@ import { RouterProvider } from "react-aria-components";
 import {
   createBrowserRouter,
   Navigate,
+  NavigateOptions,
   Outlet,
   useHref,
   useNavigate,
-  type NavigateOptions,
 } from "react-router-dom";
 
 import { Layout } from "@/containers/Layout";
@@ -40,102 +40,103 @@ function Root() {
   );
 }
 
-export const router = createBrowserRouter([
-  {
-    path: `/auth/:provider/callback`,
-    lazy: () => import("./pages/AuthCallback"),
-  },
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "/:accountSlug/:projectName/builds/:buildNumber",
-        lazy: () => import("./pages/Build"),
-      },
-      {
-        path: "/:accountSlug/:projectName/builds/:buildNumber/:diffId",
-        lazy: () => import("./pages/Build"),
-      },
-      {
-        path: "/",
-        lazy: () => import("./pages/Home"),
-      },
-      {
-        path: "/",
-        element: (
-          <Layout>
-            <Outlet />
-          </Layout>
-        ),
-        children: [
-          {
-            path: "new",
-            lazy: () => import("./pages/NewProject"),
-          },
-          {
-            path: "login",
-            lazy: () => import("./pages/Login"),
-          },
-          {
-            path: "signup",
-            lazy: () => import("./pages/Signup"),
-          },
-          {
-            path: "invite/:inviteToken",
-            lazy: () => import("./pages/Invite"),
-          },
-          {
-            path: "teams/new",
-            lazy: () => import("./pages/NewTeam"),
-          },
-          {
-            path: ":accountSlug/:projectName",
-            lazy: () => import("./pages/Project"),
-            children: [
-              {
-                index: true,
-                lazy: () => import("./pages/Project/Builds"),
-              },
-              {
-                path: "reference",
-                lazy: () => import("./pages/Project/Reference"),
-              },
-              {
-                path: "builds",
-                element: <Navigate to=".." replace={true} />,
-              },
-              {
-                path: "settings",
-                lazy: () => import("./pages/Project/Settings"),
-              },
-            ],
-          },
-          {
-            id: "account",
-            path: ":accountSlug",
-            lazy: () => import("./pages/Account"),
-            children: [
-              {
-                index: true,
-                lazy: () => import("./pages/Account/Projects"),
-              },
-              {
-                path: "new",
-                lazy: () => import("./pages/Account/NewProject"),
-              },
-              {
-                path: "settings",
-                lazy: () => import("./pages/Account/Settings"),
-              },
-            ],
-          },
-          {
-            path: "*",
-            element: <NotFound />,
-          },
-        ],
-      },
-    ],
-  },
-]);
+export const router: ReturnType<typeof createBrowserRouter> =
+  createBrowserRouter([
+    {
+      path: `/auth/:provider/callback`,
+      lazy: () => import("./pages/AuthCallback"),
+    },
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "/:accountSlug/:projectName/builds/:buildNumber",
+          lazy: () => import("./pages/Build"),
+        },
+        {
+          path: "/:accountSlug/:projectName/builds/:buildNumber/:diffId",
+          lazy: () => import("./pages/Build"),
+        },
+        {
+          path: "/",
+          lazy: () => import("./pages/Home"),
+        },
+        {
+          path: "/",
+          element: (
+            <Layout>
+              <Outlet />
+            </Layout>
+          ),
+          children: [
+            {
+              path: "new",
+              lazy: () => import("./pages/NewProject"),
+            },
+            {
+              path: "login",
+              lazy: () => import("./pages/Login"),
+            },
+            {
+              path: "signup",
+              lazy: () => import("./pages/Signup"),
+            },
+            {
+              path: "invite/:inviteToken",
+              lazy: () => import("./pages/Invite"),
+            },
+            {
+              path: "teams/new",
+              lazy: () => import("./pages/NewTeam"),
+            },
+            {
+              path: ":accountSlug/:projectName",
+              lazy: () => import("./pages/Project"),
+              children: [
+                {
+                  index: true,
+                  lazy: () => import("./pages/Project/Builds"),
+                },
+                {
+                  path: "reference",
+                  lazy: () => import("./pages/Project/Reference"),
+                },
+                {
+                  path: "builds",
+                  element: <Navigate to=".." replace={true} />,
+                },
+                {
+                  path: "settings",
+                  lazy: () => import("./pages/Project/Settings"),
+                },
+              ],
+            },
+            {
+              id: "account",
+              path: ":accountSlug",
+              lazy: () => import("./pages/Account"),
+              children: [
+                {
+                  index: true,
+                  lazy: () => import("./pages/Account/Projects"),
+                },
+                {
+                  path: "new",
+                  lazy: () => import("./pages/Account/NewProject"),
+                },
+                {
+                  path: "settings",
+                  lazy: () => import("./pages/Account/Settings"),
+                },
+              ],
+            },
+            {
+              path: "*",
+              element: <NotFound />,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
