e881823641430f8a0156e5a6e454635933541e15
Greg Bergé <berge.greg@gmail.com>
2024-10-27 16:51:10 +0100

    docs: remove examples
    Move them to argos-javascript repo

--- examples/remix/tests/homepage.spec.ts
+++
@@ -1,7 +0,0 @@
-import { argosScreenshot } from "@argos-ci/playwright";
-import { test } from "@playwright/test";
-
-test("screenshot homepage", async ({ page }) => {
-  await page.goto("http://localhost:3000");
-  await argosScreenshot(page, "homepage");
-});
