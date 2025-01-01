8b1bb9b9663dddabba211aec10eaa32668228a22
Greg Bergé <berge.greg@gmail.com>
2025-01-01 18:44:39 +0100

    chore: add logs to understand GitHub OAuth failure
    Relative to ARGOS-SERVER-XR5

--- apps/backend/src/github/auth.ts
+++ apps/backend/src/github/auth.ts
@@ -1,6 +1,7 @@
-import * as Sentry from "@sentry/node";
 import axios from "axios";
 import { z } from "zod";
+
+import logger from "@/logger";
 
 const RetrieveTokenResponseSchema = z.object({
   access_token: z.string(),
@@ -14,36 +15,32 @@ export async function retrieveOAuthToken(args: {
   code: string;
   redirectUri: string;
 }) {
-  return Sentry.withScope(async (scope) => {
-    const body = {
-      client_id: args.clientId,
-      client_secret: args.clientSecret,
-      code: args.code,
-      redirect_uri: args.redirectUri,
-    };
+  const body = {
+    client_id: args.clientId,
+    client_secret: args.clientSecret,
+    code: args.code,
+    redirect_uri: args.redirectUri,
+  };
 
-    scope.setExtra("body", body);
-
-    const result = await axios.post(
-      "https://github.com/login/oauth/access_token",
-      body,
-      {
-        headers: {
-          accept: "application/json",
-        },
+  const result = await axios.post(
+    "https://github.com/login/oauth/access_token",
+    body,
+    {
+      headers: {
+        accept: "application/json",
       },
-    );
+    },
+  );
 
-    try {
-      return RetrieveTokenResponseSchema.parse(result.data);
-    } catch (error) {
-      scope.setExtra("errorResponse", {
-        status: result.status,
-        data: result.data,
-      });
-      throw new Error("Failed to parse GitHub OAuth response", {
-        cause: error,
-      });
-    }
-  });
+  try {
+    return RetrieveTokenResponseSchema.parse(result.data);
+  } catch (error) {
+    logger.info("GitHub OAuth response errored", {
+      status: result.status,
+      data: result.data,
+    });
+    throw new Error("Failed to parse GitHub OAuth response", {
+      cause: error,
+    });
+  }
 }
