4bd8b580b028a2397d55c8e4d2e12d748157501d
Greg Bergé <berge.greg@gmail.com>
2025-05-06 09:06:21 +0200

    chore: fix redis permissions

--- apps/backend/src/util/redis/index.ts
+++ apps/backend/src/util/redis/index.ts
@@ -20,6 +20,7 @@ export const redisClient: RedisClientType = createClient({
       ? {
           tls: true,
           host: redisURL.hostname,
+          rejectUnauthorized: true,
         }
       : { tls: false },
 });
