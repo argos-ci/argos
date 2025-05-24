c98de7aee012cdb6899cf227f7e8f5f544297ca5
Greg Bergé <berge.greg@gmail.com>
2025-05-24 21:30:59 +0200

    chore: put sqids alphabet in config

--- apps/backend/src/util/sqids.ts
+++ apps/backend/src/util/sqids.ts
@@ -1,9 +1,11 @@
 import Sqids from "sqids";
 
+import config from "@/config";
+
 export const sqids = new Sqids({
   // Use a custom alphabet to avoid ambiguous characters.
   // This alphabet is URL-safe and case-insensitive.
   // This alphabet should never change, as it would break existing IDs.
-  alphabet: "FN5LTPGRW98EB2X3ZYHDCU0AS7KJQV64MO1I",
+  alphabet: config.get("sqids.alphabet"),
   minLength: 4,
 });
