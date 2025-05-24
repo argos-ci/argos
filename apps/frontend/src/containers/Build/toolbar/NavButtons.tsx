07c780645cb6a05703e497aeef426cb82f3b597d
Greg Bergé <berge.greg@gmail.com>
2025-05-24 21:30:38 +0200

    chore: remove useless memo

--- apps/frontend/src/containers/Build/toolbar/NavButtons.tsx
+++ apps/frontend/src/containers/Build/toolbar/NavButtons.tsx
@@ -1,42 +1,43 @@
-import { memo } from "react";
 import { ArrowDownIcon, ArrowUpIcon } from "lucide-react";
 
 import { useBuildHotkey } from "@/containers/Build/BuildHotkeys";
 import { HotkeyTooltip } from "@/ui/HotkeyTooltip";
 import { IconButton } from "@/ui/IconButton";
 
-export const NextButton = memo(
-  (props: { onPress: () => void; isDisabled: boolean }) => {
-    const { onPress, isDisabled } = props;
-    const hotkey = useBuildHotkey("goToNextDiff", onPress, {
-      preventDefault: true,
-      enabled: !isDisabled,
-      allowInInput: true,
-    });
-    return (
-      <HotkeyTooltip description={hotkey.description} keys={hotkey.displayKeys}>
-        <IconButton isDisabled={isDisabled} onPress={onPress}>
-          <ArrowDownIcon />
-        </IconButton>
-      </HotkeyTooltip>
-    );
-  },
-);
+export function NextButton(props: {
+  onPress: () => void;
+  isDisabled: boolean;
+}) {
+  const { onPress, isDisabled } = props;
+  const hotkey = useBuildHotkey("goToNextDiff", onPress, {
+    preventDefault: true,
+    enabled: !isDisabled,
+    allowInInput: true,
+  });
+  return (
+    <HotkeyTooltip description={hotkey.description} keys={hotkey.displayKeys}>
+      <IconButton isDisabled={isDisabled} onPress={onPress}>
+        <ArrowDownIcon />
+      </IconButton>
+    </HotkeyTooltip>
+  );
+}
 
-export const PreviousButton = memo(
-  (props: { onPress: () => void; isDisabled: boolean }) => {
-    const { onPress, isDisabled } = props;
-    const hotkey = useBuildHotkey("goToPreviousDiff", onPress, {
-      preventDefault: true,
-      enabled: !isDisabled,
-      allowInInput: true,
-    });
-    return (
-      <HotkeyTooltip description={hotkey.description} keys={hotkey.displayKeys}>
-        <IconButton isDisabled={isDisabled} onPress={onPress}>
-          <ArrowUpIcon />
-        </IconButton>
-      </HotkeyTooltip>
-    );
-  },
-);
+export function PreviousButton(props: {
+  onPress: () => void;
+  isDisabled: boolean;
+}) {
+  const { onPress, isDisabled } = props;
+  const hotkey = useBuildHotkey("goToPreviousDiff", onPress, {
+    preventDefault: true,
+    enabled: !isDisabled,
+    allowInInput: true,
+  });
+  return (
+    <HotkeyTooltip description={hotkey.description} keys={hotkey.displayKeys}>
+      <IconButton isDisabled={isDisabled} onPress={onPress}>
+        <ArrowUpIcon />
+      </IconButton>
+    </HotkeyTooltip>
+  );
+}
