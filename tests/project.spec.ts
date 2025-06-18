3f10b568133765acf69b28c7aff33033ac745bce
Jeremy Sfez <jeremy@smooth-code.com>
2025-06-18 12:38:17 +0200

    test: screenshot automations

--- tests/project.spec.ts
+++ tests/project.spec.ts
@@ -7,6 +7,13 @@ test("project builds", async ({ page }) => {
   await argosScreenshot(page, `project-builds`);
 });
 
+test("project automations", async ({ page }) => {
+  await page.goto("/smooth/big/automations");
+  await argosScreenshot(page, `project-automations`);
+  await page.goto("/smooth/big/automations/new");
+  await argosScreenshot(page, `project-automations-new`);
+});
+
 test.skip("project tests", async ({ page }) => {
   await page.goto("/smooth/big/tests");
   await expect(page.getByText("penelope.jpg")).toBeVisible();
