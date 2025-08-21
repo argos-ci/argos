abe6c8ab7fbb5460ffcec1091b372894b4a7ae06
Greg Bergé <berge.greg@gmail.com>
2025-08-21 13:01:19 -0400

    fix(build): fix missing finalizedAt
    Use the same `finalizeBuild` even for single update

--- apps/backend/src/build/finalizeBuild.ts
+++ apps/backend/src/build/finalizeBuild.ts
@@ -1,5 +1,6 @@
 import { ref, TransactionOrKnex, type PartialModelObject } from "objection";
 
+import { transaction } from "@/database";
 import { Build, BuildShard, Screenshot } from "@/database/models/index.js";
 import { BuildMetadata } from "@/database/schemas";
 import { ARGOS_STORYBOOK_SDK_NAME } from "@/util/argos-sdk";
@@ -7,7 +8,7 @@ import { ARGOS_STORYBOOK_SDK_NAME } from "@/util/argos-sdk";
 /**
  * Check if the bucket is valid from the metadata.
  */
-export function checkIsBucketValidFromMetadata(metadata: BuildMetadata | null) {
+function checkIsBucketValidFromMetadata(metadata: BuildMetadata | null) {
   return !metadata?.testReport || metadata.testReport.status === "passed";
 }
 
@@ -74,27 +75,43 @@ function aggregateMetadata(allMetatada: (BuildMetadata | null)[]) {
  * - Update the compare bucket with the screenshot count and validity.
  */
 export async function finalizeBuild(input: {
-  trx?: TransactionOrKnex;
+  /**
+   * Build to finalize.
+   */
   build: Build;
+  /**
+   * Options passed when the build is a single one, it means no shards are involved.
+   */
+  single?: {
+    metadata: BuildMetadata | null;
+    screenshots: { all: number; storybook: number };
+  };
+  /**
+   * Transaction object for database operations.
+   */
+  trx?: TransactionOrKnex;
 }) {
-  const { trx, build } = input;
+  const { trx, build, single } = input;
   const countQuery = Screenshot.query(trx).where(
     "screenshotBucketId",
     build.compareScreenshotBucketId,
   );
   const [screenshotCount, storybookScreenshotCount, shards] = await Promise.all(
     [
-      countQuery.resultSize(),
-      countQuery
-        .clone()
-        .where(ref("metadata:sdk.name").castText(), ARGOS_STORYBOOK_SDK_NAME)
-        .resultSize(),
-      BuildShard.query(trx).select("metadata").where("buildId", build.id),
+      single?.screenshots.all ?? countQuery.resultSize(),
+      single?.screenshots.storybook ??
+        countQuery
+          .clone()
+          .where(ref("metadata:sdk.name").castText(), ARGOS_STORYBOOK_SDK_NAME)
+          .resultSize(),
+      single
+        ? []
+        : BuildShard.query(trx).select("metadata").where("buildId", build.id),
     ],
   );
 
   const valid =
-    shards.length > 0
+    !single && shards.length > 0
       ? shards.every((shard) => checkIsBucketValidFromMetadata(shard.metadata))
       : checkIsBucketValidFromMetadata(build.metadata);
 
@@ -102,19 +119,23 @@ export async function finalizeBuild(input: {
     finalizedAt: new Date().toISOString(),
   };
 
-  if (shards.length > 0) {
+  if (single) {
+    buildData.metadata = single.metadata;
+  } else if (shards.length > 0) {
     buildData.metadata = aggregateMetadata(
       shards.map((shard) => shard.metadata),
     );
   }
 
-  await Promise.all([
-    build.$clone().$query(trx).patch(buildData),
-    build.$relatedQuery("compareScreenshotBucket", trx).patch({
-      complete: true,
-      screenshotCount,
-      storybookScreenshotCount,
-      valid,
-    }),
-  ]);
+  await transaction(trx, async (trx) => {
+    await Promise.all([
+      build.$clone().$query(trx).patch(buildData),
+      build.$relatedQuery("compareScreenshotBucket", trx).patch({
+        complete: true,
+        screenshotCount,
+        storybookScreenshotCount,
+        valid,
+      }),
+    ]);
+  });
 }
