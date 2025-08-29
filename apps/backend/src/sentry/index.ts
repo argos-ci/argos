98759c3bb87b59007cb8c2925482bae34cca6289
Greg Bergé <berge.greg@gmail.com>
2025-08-29 08:05:27 +0200

    chore: set level of info for 4xx errors
    Fix ARGOS-SERVER-Y76

--- apps/backend/src/sentry/index.ts
+++ apps/backend/src/sentry/index.ts
@@ -1,11 +1,23 @@
 import * as Sentry from "@sentry/node";
 
 import config from "@/config/index.js";
+import { HTTPError } from "@/web/util";
 
 export function setup() {
   Sentry.init({
     dsn: config.get("sentry.serverDsn"),
     environment: config.get("sentry.environment"),
     release: config.get("releaseVersion"),
+    beforeSend(event, hint) {
+      const error = hint.originalException;
+      if (error instanceof HTTPError) {
+        // Set level to info for 4xx errors
+        if (error.statusCode >= 400 && error.statusCode < 500) {
+          event.level = "info";
+          return event;
+        }
+      }
+      return event;
+    },
   });
 }
