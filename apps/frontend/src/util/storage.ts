0228de0d116716f605b3fa51a33d82772080fd8c
Greg Bergé <berge.greg@gmail.com>
2025-08-29 07:57:48 +0200

    fix: fix unavailable localStorage
    Fix ARGOS-BROWSER-D1

--- apps/frontend/src/util/storage.ts
+++ apps/frontend/src/util/storage.ts
@@ -5,10 +5,10 @@ import { checkIsSecurityError } from "./security-error";
  */
 export function getItem(key: string): string | null {
   try {
-    if (typeof localStorage !== "undefined") {
+    if (typeof localStorage !== "undefined" && localStorage) {
       return localStorage.getItem(key);
     }
-    if (typeof sessionStorage !== "undefined") {
+    if (typeof sessionStorage !== "undefined" && sessionStorage) {
       return sessionStorage.getItem(key);
     }
     return null;
@@ -25,11 +25,11 @@ export function getItem(key: string): string | null {
  */
 export function setItem(key: string, value: string): boolean {
   try {
-    if (typeof localStorage !== "undefined") {
+    if (typeof localStorage !== "undefined" && localStorage) {
       localStorage.setItem(key, value);
       return true;
     }
-    if (typeof sessionStorage !== "undefined") {
+    if (typeof sessionStorage !== "undefined" && sessionStorage) {
       sessionStorage.setItem(key, value);
       return true;
     }
