cd5847cecdac98195f806401054139ca9decb51e
Greg Bergé <berge.greg@gmail.com>
2025-09-23 13:09:51 +0200

    fix(invite): ignore if user is already part of the team
    Fix ARGOS-SERVER-Y7N

--- apps/backend/src/graphql/definitions/Team.ts
+++ apps/backend/src/graphql/definitions/Team.ts
@@ -871,11 +871,14 @@ export const resolvers: IResolvers = {
         await transaction(async (trx) => {
           await Promise.all([
             // Add the user to the team.
-            TeamUser.query(trx).insert({
-              userId: user.id,
-              teamId: team.id,
-              userLevel: teamInvite.userLevel,
-            }),
+            TeamUser.query(trx)
+              .insert({
+                userId: user.id,
+                teamId: team.id,
+                userLevel: teamInvite.userLevel,
+              })
+              .onConflict()
+              .ignore(),
             // Delete the invite.
             teamInvite.$query(trx).delete(),
           ]);
