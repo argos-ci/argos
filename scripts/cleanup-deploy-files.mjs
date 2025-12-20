87a3f509fcb6185da8e6cbd3be9523e8e3282853
Greg Bergé <berge.greg@gmail.com>
2025-12-20 12:02:26 +0100

    Revert "chore: replace .slugignore with .deployignore"
    This reverts commit c960d02ab520e59181b4dc97e1b540bf7dbc7216.

--- scripts/cleanup-deploy-files.mjs
+++
@@ -1,7 +0,0 @@
-import { readFile } from "node:fs/promises";
-import { deleteAsync } from "del";
-
-const lines = await readFile(".deployignore", "utf8");
-const files = lines.split("\n").filter(Boolean);
-
-await deleteAsync(files);
