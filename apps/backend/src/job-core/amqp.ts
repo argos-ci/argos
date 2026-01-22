850d85c2b6e948d3653163fd47ae9f80555bf7e0
Greg Bergé <berge.greg@gmail.com>
2026-01-22 03:33:18 +0100

    refactor: fix error handling at creation

--- apps/backend/src/job-core/amqp.ts
+++ apps/backend/src/job-core/amqp.ts
@@ -5,15 +5,15 @@ import config from "@/config";
 let connectPromise: ReturnType<typeof amqp.connect> | null = null;
 export async function connect() {
   if (!connectPromise) {
-    connectPromise = amqp.connect(config.get("amqp.url"));
+    connectPromise = amqp.connect(config.get("amqp.url")).catch((error) => {
+      // If there is an error avoid caching the connection
+      // to be able to reconnect properly.
+      connectPromise = null;
+      throw error;
+    });
   }
 
-  return connectPromise.catch((error) => {
-    // If there is an error avoid caching the connection
-    // to be able to reconnect properly.
-    connectPromise = null;
-    throw error;
-  });
+  return connectPromise;
 }
 
 export async function quitAmqp() {
