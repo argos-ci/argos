5ca7d4c0e4c9f5329971772dc84e57cd10dfdeb1
Greg Bergé <berge.greg@gmail.com>
2026-03-18 13:19:59 +0100

    chore: put rate limit in config (#2056)

--- apps/backend/src/web/api/v2.ts
+++ apps/backend/src/web/api/v2.ts
@@ -2,15 +2,16 @@ import { Router } from "express";
 import { rateLimit } from "express-rate-limit";
 
 import { openAPIRouter } from "@/api";
+import config from "@/config";
 import { createRedisStore } from "@/util/rate-limit";
 
 const router: Router = Router();
 
 const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 500, // Limit each IP to 500 requests per `window` (here, per 5 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  windowMs: config.get("api.rateLimit.window"),
+  limit: config.get("api.rateLimit.limit"),
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
   store: createRedisStore("api"),
 });
 
