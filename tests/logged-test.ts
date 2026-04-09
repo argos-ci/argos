3bc384ce06518721405c18d58cc3a255c223a5dc
Greg Bergé <berge.greg@gmail.com>
2026-04-09 09:19:01 +0200

    Fix logged test

--- tests/logged-test.ts
+++ tests/logged-test.ts
@@ -1,22 +1,35 @@
+/* eslint-disable no-empty-pattern */
 import { type Page } from "@playwright/test";
 
 import { createJWT, JWT_VERSION } from "../apps/backend/src/auth/jwt";
 import argosConfig from "../apps/backend/src/config";
+import { Account, type User } from "../apps/backend/src/database/models";
 import { seedTest } from "./seed-test";
 
-export const loggedTest = seedTest.extend<{
-  page: Page;
-}>({
-  page: async ({ page, user }, use) => {
+export const loggedTest = seedTest.extend<
+  {
+    page: Page;
+  },
+  {
+    auth: { account: Account; user: User };
+  }
+>({
+  auth: [
+    async ({ user }, use) => {
+      await use(user);
+    },
+    { scope: "worker" },
+  ],
+  page: async ({ page, auth }, use) => {
     await page.context().addCookies([
       {
         name: "argos_jwt",
         value: createJWT({
           version: JWT_VERSION,
           account: {
-            id: user.account.id,
-            name: user.account.name,
-            slug: user.account.slug,
+            id: auth.account.id,
+            name: auth.account.name,
+            slug: auth.account.slug,
           },
         }),
         url: argosConfig.get("server.url"),
